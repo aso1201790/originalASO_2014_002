543849a5ace20afa3110400b4079c579ad09560a
student <student@PC111S05.fgakusei.local>
2014-06-20 11:04:41 +0900

    できましたー

--- src/com/example/originalaso_2014_002/MaintenanceActivity.java
+++ src/com/example/originalaso_2014_002/MaintenanceActivity.java
@@ -11,6 +11,7 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 
 
@@ -46,6 +47,25 @@ public class MaintenanceActivity extends Activity implements View.OnClickListene
 	public void onClick(View v) {
 		// TODO 自動生成されたメソッド・スタブ
 		switch(v.getId()){
+		case R.id.btnSakujo: //削除ボタンが押された
+
+			// 選択行があれば
+			if(this.selectedID != -1){
+				this.deleteFromHitokoto(this.selectedID);
+				ListView lstHitokoto = (ListView)findViewById(R.id.LvHITOKOTO);
+				// ListViewにDBをセット
+				this.setDBValuetoList(lstHitokoto);
+				// 選択行を忘れる
+				this.selectedID = -1;
+				this.lastPosition = -1;
+			}
+			else{
+				// なければ、トースト(簡易メッセージ)を表示
+				Toast.makeText(MaintenanceActivity.this,"削除する行を選んでください", Toast.LENGTH_SHORT).show();
+
+				}
+		break;
+
 		case R.id.btnCancel:
 			finish();
 			break;
@@ -54,11 +74,8 @@ public class MaintenanceActivity extends Activity implements View.OnClickListene
 	}
 
 
-	@Override
-	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-		// TODO 自動生成されたメソッド・スタブ
 
-	}
+
 	/**
 	 * 引数のListViewにDBのデータをセット
 	 * @param lstHitokoto 対象となるListView
@@ -91,4 +108,53 @@ public class MaintenanceActivity extends Activity implements View.OnClickListene
 
 		lstHitokoto.setAdapter(adapter);
 	}
+	/**
+	 *  Hitokotoテーブルから、引数で指定した「_id」と同じ値を持つレコードを削除
+	 *  @param id 指定する値
+	 */
+	private void deleteFromHitokoto(int id){
+		// クラスのフィールド変数がNULLなら、データベース空間オープン
+		if(sdb == null) {
+			helper = new MySQLiteOpenHelper(getApplicationContext());
+		}
+		try{
+			sdb = helper.getWritableDatabase();
+		}catch(SQLiteException e){
+			// 異常終了
+			Log.e("ERROR", e.toString());
+		}
+		// MySQLiteOpenHelperにDELETE文を実行させる
+		this.helper.deleteHitokoto(sdb, id);
+	}
+
+
+	/**
+	 * @param AdapterView<?> parent クリックしたListView
+	 * @param View view クリックしたListViewの中の各行
+	 * @param int position 何行目をクリックしたか
+	 * @param long viewid 未使用
+	 */
+
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position, long viewid) {
+		// TODO　自動生成されたメソッド・スタブ
+
+		// 前に選択中の行があれば、背景色を透明にする
+		if(this.selectedID!=-1){
+			parent.getChildAt(this.lastPosition).setBackgroundColor(0);
+
+		}
+		// 選択中の行の背景色をグレーにする
+		view.setBackgroundColor(android.graphics.Color.LTGRAY);
+
+		// 選択行のレコードを指し示すカーソルを取得
+		SQLiteCursor cursor = (SQLiteCursor)parent.getItemAtPosition(position);
+		// カーソルのレコードから、「_id」の値を取得して記憶
+		this.selectedID = cursor.getInt(cursor.getColumnIndex("_id"));
+		// 何行目を選択したかも記憶
+		this.lastPosition = position;
+	}
+
+
+
 }
